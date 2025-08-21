2cc14a21cfdab9ac345efd549e61d5f72b8afb78
Anni <anni.alanen@metropolia.fi>
2025-08-21 14:08:38 +0300

    vitttuuuu

---
+++ moduuli1/src/Testi.java
@@ -0,0 +1,9 @@
+public class Testi {
+    public static void main(String[] args) {
+        System.out.println("Hello You");
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
