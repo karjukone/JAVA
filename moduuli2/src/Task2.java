9afbebb450852655f6319bdd1ac2942da24acd4f
Anni <anni.alanen@metropolia.fi>
2025-08-21 16:41:35 +0300

    jou

---
+++ moduuli2/src/Task2.java
@@ -0,0 +1,18 @@
+/* formula for fahrenheit to celcius convertion: (F - 32)/ 1.8    */
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a temperature in Fahrenheit: ");
+        double temp = Double.parseDouble(scanner.nextLine());
+
+        double celcius = (temp - 32) / 1.8;
+
+        System.out.printf("The temperature in Celsius %6.1f", celcius);
+
+        scanner.close();
+    }
+}
